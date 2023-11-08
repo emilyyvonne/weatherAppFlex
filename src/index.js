54047fd3f8eea739acbc4bd5e6e41b2dba0ac4bb
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2023-11-08 12:46:07 -0500

    adjustong forecast css

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHtml =
         forecastHtml +
         `
@@ -81,7 +81,7 @@ function displayForecast(response) {
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º   |</strong>
+            <strong>${Math.round(day.temperature.maximum)}º |</strong>
           </div>
           <div class="weather-forecast-temperature">${Math.round(
             day.temperature.minimum
